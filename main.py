bca85c7da7f0c4330156e92b46537164cd0ad85e
kevinxdg <kevinxdg@gmail.com>
2022-11-02 23:25:39 +0800

    First GIT for this pro.

--- main.py
+++ main.py
@@ -2,22 +2,28 @@
 
 import arcpy
 import numpy as np
+
 file_path = r'H:\Python\Data\2000年草地FVC.tif'
 
-ras = arcpy.Raster(file_path)
-arr = arcpy.RasterToNumPyArray(ras).tolist()
+ras = arcpy.Raster(file_path)    # 读取栅格图片
+arr = arcpy.RasterToNumPyArray(ras) # 转为二维矩阵
+
+arr = arr.tolist()   # 一维向量
 
 lst = [i for item in arr for i in item]
 
-print(lst)
+#print(lst)
 
 
-def get_list_by_values(self, value_min, value_max):
-    tmp_list = []
-    for value in self.to_list:
-        if (value >= value_min) and (value <= value_max):
-            tmp_list.append(value)
-    return tmp_list
+vmin = -1
+vmax = 1
+tmplst = []
+for value in lst:
+    if (value >= vmin) and (value <= vmax):
+        tmplst.append(value)
+#print(tmplst)
 
+tmplst.sort()
+p05 = np.percentile(tmplst, 25)
+print(p05)
 
-#p05 = np.percentile(self.value_list, percent)
