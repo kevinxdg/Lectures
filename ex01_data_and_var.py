3dc730fd37d42669e5c952fd95c8d11914cb65fa
kevinxdg <kevinxdg@gmail.com>
2022-11-03 09:28:52 +0800

    First GIT for this pro.

--- ex01_data_and_var.py
+++ ex01_data_and_var.py
@@ -10,5 +10,22 @@ s = pi * r ** 2
 print(c)
 print(s)
 
+# 字符串连接
+s1 = 'This is '
+s2 = 'a string'
+s = s1 + s2
+print(s)
+
+# 关系运算
+x = 100
+r1 = (x <= 100)
+r2 = (x + 3) / 8 != 6
+print(r1)
+print(r2)
+
+# 布尔运算
+con1 = True and False
+con2 = not False or False
+print(con1 and con2)
 
 
