ec8c45c38728201860b2386d58936e2259c1f04e
kevinxdg <kevinxdg@gmail.com>
2022-11-11 17:20:53 +0800

    FromCmt

---
+++ arc01_Raster.py
@@ -0,0 +1,28 @@
+
+import arcpy
+import numpy as np
+
+file_path = r'H:\Python\Data\2000年草地FVC.tif'
+
+ras = arcpy.Raster(file_path)    # 读取栅格图片
+arr = arcpy.RasterToNumPyArray(ras) # 转为二维矩阵
+
+arr = arr.tolist()   # 一维向量
+
+lst = [i for item in arr for i in item]
+
+vmin = -1
+vmax = 1
+tmplst = []
+for value in lst:
+    if (value >= vmin) and (value <= vmax):
+        tmplst.append(value)
+
+
+tmplst.sort()
+p05 = np.percentile(tmplst, 25)
+print(p05)
+
+
+
+
