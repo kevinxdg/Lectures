8df4ec47d98bacb516f0a1ba44a6304a07ae8fdd
kevinxdg <kevinxdg@gmail.com>
2022-11-19 13:29:38 +0800

    office

--- arc02_mosaic.py
+++ arc02_mosaic.py
@@ -2,21 +2,28 @@
 
 import arcpy
 import arcpy
-import numpy as np
 import glob
 
-file_path = r'F:\ArcGIS\Data\Landuse'
+FVC_path = r'F:\ArcGIS\Data\FVC'               #  已有的分不同土地利用提取的FVC
+output_path = r'F:\ArcGIS\Data\FVCMosaic'          #  用于保存合成的FVC地址
 
-first = ['湖北1980lucc.tif','湖北1990lucc.tif','湖北1995lucc.tif']
-last = ['湖北1990lucc.tif','湖北1995lucc.tif','湖北2000lucc.tif']
+files_2000 = glob.glob1(FVC_path,'*2000*.tif')         # 获取数据地址下，名称包含2000、扩展名为.tif的文件
+full_files_2000 = [FVC_path + '\\' + f for f in files_2000]   # 组成全路径
+# 根据 镶嵌工具的语法（参考工具帮助）
+# 第1个参数是要镶嵌的所有数据
+# 第2个参数是保存数据的地址
+# 第3个参数是保存数据的名称
+# 第4个参数表示要镶嵌的波段数
+arcpy.MosaicToNewRaster_management(full_files_2000,output_path,'FVC2000.tif',number_of_bands=1)
 
-for i in range(3):
-    ras1 = arcpy.Raster(file_path + '\\' + first[i])
-    ras2 = arcpy.Raster(file_path + '\\' + last[i])
-    ras = ras1 * 100 + ras2
-    ras.save('F:\\ArcGIS\\Data\\a' + str(i) + '.tif' )
+# 以下请完成，对2020年FVC进行镶嵌
+
+print('Done')
 
 
 
 
-#arcpy.Mosaic_management()
+
+
+
+
---
+++ arc06_luc.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+
+import arcpy
+import arcpy
+import numpy as np
+import glob
+
+file_path = r'F:\ArcGIS\Data\Landuse'
+
+first = ['湖北1980lucc.tif','湖北1990lucc.tif','湖北1995lucc.tif']
+last = ['湖北1990lucc.tif','湖北1995lucc.tif','湖北2000lucc.tif']
+
+for i in range(3):
+    ras1 = arcpy.Raster(file_path + '\\' + first[i])
+    ras2 = arcpy.Raster(file_path + '\\' + last[i])
+    ras = ras1 * 100 + ras2
+    ras.save('F:\\ArcGIS\\Data\\a' + str(i) + '.tif' )
