e82f32fd1f88db18eb2dd47231697fcbaacff6e2
kevinxdg <kevinxdg@gmail.com>
2022-11-23 09:26:11 +0800

    FromCmt

--- arc02_mosaic.py
+++ arc02_mosaic.py
@@ -4,11 +4,19 @@ import arcpy
 import arcpy
 import glob
 
-FVC_path = r'F:\ArcGIS\Data\FVC'               #  已有的分不同土地利用提取的FVC
-output_path = r'F:\ArcGIS\Data\FVCMosaic'          #  用于保存合成的FVC地址
+arcpy.env.overwriteOutput = True
+
+FVC_path = r'H:\ArcGIS\Data\HBFVC'               #  已有的分不同土地利用提取的FVC
+output_path = r'H:\ArcGIS\Data\FVC'          #  用于保存合成的FVC地址
 
 files_2000 = glob.glob1(FVC_path,'*2000*.tif')         # 获取数据地址下，名称包含2000、扩展名为.tif的文件
 full_files_2000 = [FVC_path + '\\' + f for f in files_2000]   # 组成全路径
+
+
+#vtype = arcpy.GetRasterProperties_management(full_files_2000[0],'VALUETYPE')
+#print(vtype)
+
+
 # 根据 镶嵌工具的语法（参考工具帮助）
 # 第1个参数是要镶嵌的所有数据
 # 第2个参数是保存数据的地址
