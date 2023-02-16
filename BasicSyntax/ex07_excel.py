5738856ce358274173aa640d02000d12797de84e
kevinxdg <kevinxdg@gmail.com>
2023-02-16 14:19:11 +0800

    FromCmt

--- BasicSyntax/ex07_excel.py
+++ BasicSyntax/ex07_excel.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import openpyxl as xl
 import os
+import statsmodels.api as sm
+import statsmodels.tsa.stattools as stools
+
 data_dir = os.getcwd() + '\\..\\Data\\'
 data_file_a = r'a.xlsx'
 data_file_b = r'b.xlsx'
@@ -18,9 +21,19 @@ data.to_excel(data_path_a)
 # 读取 excel
 df = pd.read_excel(data_path_e)
 print(df)
-print(df.iloc[:,3:5])   # 取其中指定列
-print(df.loc[:,'陕西'])
+x = df.iloc[:,2:3]
+y = df.iloc[:,1:2]
+print(x)
+print(y)
+X = sm.add_constant(x)
+model = sm.OLS(y,X)
+result_ols = model.fit()
+print(result_ols.summary())
+#print(df.iloc[:,3:5])   # 取其中指定列
+#print(df.loc[:,'陕西'])
 
+r = stools.adfuller(y)
+print(r)
 
 # 操作 Excel
 # 新建 workbook
