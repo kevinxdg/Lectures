732022a8d6d642dcd6796c230f42e52dbcc13da5
xutianle <xutianle0221@163.com>
2023-03-01 09:38:42 +0800

    FromCmt

--- BasicSyntax/ex07_excel.py
+++ BasicSyntax/ex07_excel.py
@@ -6,16 +6,14 @@ import os
 import statsmodels.api as sm
 import statsmodels.tsa.stattools as stools
 
-data_dir = os.getcwd() + '\\..\\Data\\'
-data_file_a = r'a.xlsx'
-data_file_b = r'b.xlsx'
-data_file_e = r'example.xlsx'
-data_path_a = data_dir + data_file_a
-data_path_b = data_dir + data_file_b
-data_path_e = data_dir + data_file_e
+
+
+data_file_e = r'D:\code\Projects\Test\Lectures\XTL\Energy.xlsx'
+
+data_path_e =data_file_e
 # 直接保存 dataframe
 data = pd.DataFrame(data=[[1,2,3],[4,5,6]])
-data.to_excel(data_path_a)
+
 
 
 # 读取 excel
@@ -39,7 +37,7 @@ print(r)
 # 新建 workbook
 book_b = xl.Workbook()
 book_b.create_sheet('Lecture')
-book_b.save(data_path_b)
+
 book_b.close()
 
 # 读取 excel表格
