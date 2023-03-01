02252582cd2211656ed6009bc078870cbd4512f6
xutianle <xutianle0221@163.com>
2023-03-01 09:43:21 +0800

    FromCmt

--- XTL/FVC.py
+++ XTL/FVC.py
@@ -8,13 +8,14 @@ from arcpy.sa import *
 # Set environment settings
 env.workspace = "D:/data/HBdata/FVC变化率"
 inRaster = Raster("00_20.tif")
-if 0.2 >= inRaster >= 0:
+
+if 0.2 >= inRaster >=0:
     print(1)
-elif 0.6 >= inRaster >= 0.4:
+elif 0.6 >= inRaster >=0.4:
     print(2)
-elif 0.8 >= inRaster >= 0.6:
+elif 0.8 >= inRaster >=0.6:
     print(3)
-elif 1.0 >= inRaster >= 0.8:
+elif 1.0 >= inRaster >=0.8:
     print(4)
 
 # Check out the ArcGIS Spatial Analyst extension license
