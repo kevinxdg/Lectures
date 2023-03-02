a4e4c2d156e3878903173c33fed9e7f64e8d49d3
kevinxdg <kevinxdg@gmail.com>
2023-03-02 17:01:33 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/Lectures.iml
    #	.idea/misc.xml

--- CCG/FVC.py
+++ CCG/FVC.py
@@ -3,3 +3,6 @@ import arcpy
 
 G_FVC2000 = arcpy.Raster(r'D:\gisshuju\Changjiang\重叠年份数据03-06年\GIMMS\2003\FVC.A20030.annual.tif')
 M_FVC2020 = arcpy.Raster(r'D:\gisshuju\Changjiang\重叠年份数据03-06年\MODIS\2003\FVC.A20030.annual.tif')
+
+sub_FVC = G_FVC2000 - M_FVC2020
+sub_FVC.
