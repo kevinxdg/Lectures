157152f997bf6fb683fba2cd00e0fa38e0904650
xianhuashe <caocongge2022@163.com>
2023-03-02 22:06:21 +0800

    1

--- CCG/FVC.py
+++ CCG/FVC.py
@@ -2,6 +2,7 @@
 import arcpy
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 G_FVC2003 = arcpy.Raster(r'D:\gisshuju\Changjiang\重叠年份数据03-06年\GIMMS\2003\FVC.A20030.annual.tif')
@@ -10,8 +11,15 @@ M_FVC2003 = arcpy.Raster(r'D:\gisshuju\Changjiang\重叠年份数据03-06年\MOD
 sub_FVC = G_FVC2003 - M_FVC2003
 arr = arcpy.RasterToNumPyArray(sub_FVC)  # 转为二维矩阵
 lst = [i for item in arr for i in item]  # 将二维矩阵 arr 转换成一维向量
-#sub_FVC.
-print(lst)
-plt.hist(lst)
+vmin = -1
+vmax = 1
+tmplst = []
+for value in lst:
+    if(value >= vmin) and (value <= vmax):  # 只保留-1到1之间的数值
+        tmplst.append(value)
+
+print(tmplst)
+
+plt.hist(tmplst,bins=100)
 plt.show()
 
