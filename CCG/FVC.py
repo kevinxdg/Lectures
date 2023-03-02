ee7f381db7707f7f6b93e1dda19403061ba90d89
xianhuashe <caocongge2022@163.com>
2023-03-02 16:55:11 +0800

    1

---
+++ CCG/FVC.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+import arcpy
+
+G_FVC2000 = arcpy.Raster(r'D:\gisshuju\Changjiang\重叠年份数据03-06年\GIMMS\2003\FVC.A20030.annual.tif')
+M_FVC2020 = arcpy.Raster(r'D:\gisshuju\Changjiang\重叠年份数据03-06年\MODIS\2003\FVC.A20030.annual.tif')
