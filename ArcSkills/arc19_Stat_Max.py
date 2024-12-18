6408be63db5d88d2ef6c4f8f1ade8f905895494c
kevinxdg <kevinxdg@163.com>
2024-12-18 15:28:32 +0800

    属性表

--- ArcSkills/arc19_Stat_Max.py
+++ ArcSkills/arc19_Stat_Max.py
@@ -10,8 +10,8 @@ from arcpy.sa import *
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984 UTM Zone 49N")
 arcpy.env.overwriteOutput = True        # 保存时允许覆盖
 
-shp_file = r'F:\ArcGIS\Data\Climatatic\逐日最高气温\【立方数据学社】%d年逐日最高气温.shp' % 1981
-
-output_file = r'F:\ArcGIS\Data\Climatatic\最高气温属性表\%d年逐日最高气温.xlsx' % 1981
-
-arcpy.TableToExcel_conversion(shp_file, output_file)
+for year in range(1981,2024):
+    shp_file = r'F:\ArcGIS\Data\Climatatic\逐日最高气温\【立方数据学社】%d年逐日最高气温.shp' % year
+    output_file = r'F:\ArcGIS\Data\Climatatic\最高气温属性表\%d年逐日最高气温.xlsx' % year
+    arcpy.TableToExcel_conversion(shp_file, output_file)
+    print(r'[%d]-完成导出' % year)
