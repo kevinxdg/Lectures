9b04a22f5f15b97190f7c568050c79552d6c7443
kevinxdg <kevinxdg@163.com>
2024-12-19 16:00:20 +0800

    统计无霜期

--- BasicSyntax/ex16_period.py
+++ BasicSyntax/ex16_period.py
@@ -1,6 +1,12 @@
 #
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
+
+data_excel_file = r'F:\ArcGIS\Data\Climatatic\最高气温属性表\%d年逐日最高气温.xlsx'
+output_file = r'F:\ArcGIS\Data\Climatatic\统计\无霜期.xlsx'
+
+prompt_info1 = r'[%d] 已统计'
+prompt_info2 =r'结果已保存至 %s' % output_file
 
 #定义函数，确定指定日期是当年的第几天
 def day_of_year(year, month, day):
@@ -9,38 +15,54 @@ def day_of_year(year, month, day):
     delta = my_day - first_day
     return delta.days
 
-def data_lessthanzero(data):
-    return data.lt(0).astype(int)
+def data_lessthanzero(data, value=0):
+    return data.lt(value).astype(int)
 
-
-
-def period(year,data_l):
-    June30_index = day_of_year(year, 6, 30)
-    city_count = 372
-    for i in range(city_count):
-        days = June30_index + 1
-        total_days = day_of_year(year,12,31)
-        last_days = total_days -June30_index
-        start = 0
-        end_date = total_days
-        for j in range(days):
-            value = data_l.iloc[i,June30_index - j]
-            if value == 1:
-                start = June30_index - j + 1
+def start_end_day(year, data_l):
+    First_day = datetime(year, 1, 1)
+    June30 = day_of_year(year, 6, 30)
+    rows, cols = data_l.shape
+    period_df = pd.DataFrame(index=range(rows), columns=['start_date', 'end_date', str(year)])
+    for i in range(rows):
+        start_day = 0
+        end_day = day_of_year(year, 12, 31)
+        for j in range(June30+1):
+            index = June30 - j
+            if data_l.iloc[i,index] == 1:
+                start_day = index + 1
                 break
-
-        for j in range(last_days):
-            value = data_l.iloc[i,June30_index + j]
-            if value == 1:
-                end_date = June30_index + j - 1
+        for j in range(end_day - June30 + 1):
+            index = June30 + j
+            if data_l.iloc[i,index] == 1:
+                end_day = index - 1
                 break
-        period_days = end_date - start + 1
-        print(period_days)
+        period_days = end_day - start_day + 1
+
+        start_date = First_day + timedelta(days=start_day)
+        end_date = First_day + timedelta(days=end_day)
+        period_df.iloc[i,0] = start_date.date()
+        period_df.iloc[i,1] = end_date.date()
+        period_df.iloc[i,2] = period_days
+    return period_df
+
+def cal_period():
+    period_data = pd.DataFrame([])
+    for year in range(1981, 2024):
+        excel_file = data_excel_file % year
+        days = day_of_year(year, 12, 31)
+        data = pd.read_excel(excel_file, usecols=range(7, days + 8))
+        data_l = data_lessthanzero(data)
+        period_df = start_end_day(year, data_l)
+        period_data = pd.concat([period_data, period_df[str(year)]], axis=1)
+        print(prompt_info1 % year)
+    print(period_data)
+    period_data.to_excel(output_file, index=True)
+    print(prompt_info2)
 
 
 
 
-excel_file = r'F:\ArcGIS\Data\Climatatic\最高气温属性表\%d年逐日最高气温.xlsx' % 1981
-data = pd.read_excel(excel_file, usecols="H:NH")
-data_l = data_lessthanzero(data)
-print(period(1981,data_l))
+
+
+cal_period()
+#print(period_data)
